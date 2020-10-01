afd61bc5367c8285a98519ae28e7902ea5db7dcb
gzukowskiwork@gmail.com <gzukowskiwork@gmail.com>
2020-10-01 22:49:07 +0200

    Identity roles

---
+++ CookBookMVC/Installers/Extensions/ConnectionStringService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookBookMVC.Installers.Extensions
+
+{
+    public static class ConnectionStringService
+    {
+        public static string GetConnectionString(this IConfiguration configuration, string name)
+        {
+            return configuration?.GetSection("connection")?[name];
+        }
+    }
+}
---
+++ CookBookMVC/Installers/Services/IdentityService.cs
@@ -0,0 +1,17 @@
+﻿using CpntextLib.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Models;
+
+namespace CookBookMVC.Installers.Services
+{
+    public class IdentityService : IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<CookBookContext>();
+        }
+    }
+}
